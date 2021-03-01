95ed8dfce76d493457549c2bf12ab588a54f4fb2
France900 <francesco.em.livio@gmail.com>
2021-03-01 18:03:23 +0100

    Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,26 @@
 // età
 var anni = parseInt(prompt("quanti hanni hai?", "anni"));
+document.getElementById("anni").innerHTML = anni; // trasporta scritte in js in html
 
-if (isNaN(anni)) {
-  console.log("devi inserire un numero!");
-  document.getElementById("sorry").innerHTML = "Mi dispiace ma se non inserisci un numero non funziona"; // trasporta scritte in js in html
-}
-
+//Kilometri
 var kiloMetri = parseInt(prompt("quanti Km vuoi fare?", "Km"));
+document.getElementById("kM").innerHTML = kiloMetri; // trasporta scritte in js in html
 
+// prezzi
 var prezzoKm = 0.21;
 var prezzoTot = prezzoKm * kiloMetri;
 
-if (anni < 18) {
-  document.getElementById("").innerHTML = "Il Prezzo del tuo biglietto è: " + anni * prezzoTot / 100 * 80 + " Euro!"; // trasporta scritte in js in html
-} else if (anni > 65) {
-  document.getElementById("").innerHTML = "Il Prezzo del tuo biglietto è: " + anni * prezzoTot / 100 * 60 + " Euro!"; // trasporta scritte in js in html
-} else {
-  document.getElementById("").innerHTML = "Il Prezzo del tuo biglietto è: " + prezzoTot + " Euro!"; // trasporta scritte in js in html
+//logica
+if (isNaN(anni)) {
+  document.getElementById("sorry").innerHTML = "Mi dispiace ma se non inserisci la tua età non funziona"; // trasporta scritte in js in html
+} else if (isNaN(kiloMetri)) {
+  document.getElementById("sorry").innerHTML = "Mi dispiace ma se non inserisci un numero di Km non funziona"; // trasporta scritte in js in html
+} else if (!isNaN(anni)) {
+  if (anni < 18) {
+    document.getElementById("prezzofinale").innerHTML = (prezzoTot / 100 * 80).toFixed(2) + " Euro!"; // trasporta scritte in js in html
+  } else if (anni > 65) {
+    document.getElementById("prezzofinale").innerHTML = (prezzoTot / 100 * 60).toFixed(2) + " Euro!"; // trasporta scritte in js in html
+  } else {
+    document.getElementById("prezzofinale").innerHTML = prezzoTot + " Euro!"; // trasporta scritte in js in html
+  }
 }
